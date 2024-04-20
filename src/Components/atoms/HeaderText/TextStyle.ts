0a90b27003af147bafee2418d36620f57464dfbe
Victor udenna <victorudenna@gmail.com>
2024-04-21 00:02:09 +0100

    fixed deployment issue

--- src/Components/atoms/HeaderText/TextStyle.ts
+++ src/Components/atoms/HeaderText/TextStyle.ts
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
-import Colors from '../../../helpers/Colors'
 export const TextStyle = styled.h2`
-    color: ${Colors.neutral};
     font-size: 2rem;
-    font-family: Source Sans Pro
 `;
 
 export default TextStyle;
--- src/Components/atoms/Text/TextStyle.ts
+++ src/Components/atoms/Text/TextStyle.ts
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
-import Colors from '../../../helpers/Colors'
 export const TextStyle = styled.p`
-    color: ${Colors.neutral};
     font-size: 1em;
-    font-family: Tittilium Web
 `;
 
 export default TextStyle;
