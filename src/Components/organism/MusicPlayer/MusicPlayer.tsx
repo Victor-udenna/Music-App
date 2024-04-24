e7b98b113b9056cff65bdc442e766b0a4cf7f47a
Victor udenna <victorudenna@gmail.com>
2024-04-24 21:25:16 +0100

    wip

--- src/Components/organism/MusicPlayer/MusicPlayer.tsx
+++ src/Components/organism/MusicPlayer/MusicPlayer.tsx
@@ -18,27 +18,38 @@ import { useSelector } from 'react-redux'
 
 const MusicPlayer = () => {
   const data = useSelector((state: any) => state.musicData.data)
-  const audioRef = useRef<any | null>(null)
+  const audioRef = useRef<any | null>()
   const [isPlaying, setIsPlaying] = useState(false)
   const [currentTime, setCurrentTime] = useState(0)
   const [isMuted, setIsMuted] = useState(false)
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [audioLoop, setAudioLoop] = useState(false);
+  const  [autoPlay, setAutoplay] = useState(false)
 
   useEffect(() => {
     if (data && data.index !== undefined) {
-      setSelectedIndex(data.index);
+      setSelectedIndex(data.index)
     }
-  }, [data]);
+  }, [data])
 
+  // const togglePlay = () => {
+  //   if (isPlaying) {
+  //     audioRef?.current?.pause()
+  //     setIsPlaying(false)
+  //   } else {
+  //     audioRef?.current?.play()
+  //     setIsPlaying(true)
+  //   }
+  // }
 
-  const togglePlay = () => {
-    if (isPlaying) {
-      audioRef.current.pause()
-    } else {
-      audioRef.current.play()
-    }
-    setIsPlaying(!isPlaying)
+  const handlePlay =()=>{
+    audioRef?.current?.pause()
+    setIsPlaying(false)
+  }
+
+  const handlePause =()=>{
+    audioRef?.current?.play()
+    setIsPlaying(true)
   }
 
   const handleSeek = (e: any) => {
@@ -47,27 +58,48 @@ const MusicPlayer = () => {
     audioRef.current.currentTime = newTime
   }
 
+  const toggleLoop = () => {
+    setAudioLoop(!audioLoop)
+  }
+
   const toggleMute = () => {
     audioRef.current.muted = !isMuted
     setIsMuted(!isMuted)
   }
 
-
   const handleForward = () => {
-    if (selectedIndex < data.data.length - 1) {
-      setSelectedIndex((prevIndex) => prevIndex + 1);
-    } if(selectedIndex === data.data.length -1){
+    if (selectedIndex < data?.data?.length - 1) {
+      setSelectedIndex((prevIndex) => prevIndex + 1)
+    }
+    if (selectedIndex === data?.data?.length - 1) {
       setSelectedIndex(0)
     }
-  };
+  }
 
   const handleBackfoward = () => {
     if (selectedIndex > 0) {
-      setSelectedIndex((prevIndex) => prevIndex - 1);
-    } if( selectedIndex === 0){
-      setSelectedIndex(data.data.length -1)
+      setSelectedIndex((prevIndex) => prevIndex - 1)
+    }
+    if (selectedIndex === 0) {
+      setSelectedIndex(data?.data?.length - 1)
     }
   }
+
+  const moveTonextItem = () => {
+    if (
+      audioRef?.current?.currentTime === audioRef?.current?.duration &&
+      audioLoop == false
+    ) {
+      handleForward()
+      handlePlay()
+      setAutoplay(true)
+    }
+  }
+
+
+  useEffect(() => {
+    moveTonextItem()
+  }, [audioRef?.current?.currentTime])
 
   const shortenString = (str: string) => {
     const { length } = str
@@ -75,7 +107,8 @@ const MusicPlayer = () => {
   }
 
   const audioSrc = data && data?.data && data?.data[selectedIndex]?.preview
-  const albumCover = data && data?.data && data?.data[selectedIndex]?.album.cover_big
+  const albumCover =
+    data && data?.data && data?.data[selectedIndex]?.album.cover_big
   const songTitle = data && data?.data && data?.data[selectedIndex]?.title
 
   return (
@@ -84,7 +117,9 @@ const MusicPlayer = () => {
         <audio
           ref={audioRef}
           src={audioSrc}
-          onTimeUpdate={() => setCurrentTime(audioRef.current.currentTime)}
+          onTimeUpdate={() => setCurrentTime(audioRef?.current?.currentTime)}
+          loop={audioLoop}
+          autoPlay={autoPlay}
         />
         <div className="music__functionality">
           <div className="controls-container control-header">
@@ -102,9 +137,12 @@ const MusicPlayer = () => {
             >
               <FaStepBackward size={18} />
             </button>
-            <button onClick={togglePlay} className="playpause-btn">
+            {
+              isPlaying ? <button onClick={handlePlay} className="playpause-btn"><FaPause size={20} /></button> :   <button onClick={handlePause} className="playpause-btn"><FaPlay size={20} /></button>
+            }
+            {/* <button onClick={togglePlay} className="playpause-btn">
               {isPlaying ? <FaPause size={20} /> : <FaPlay size={20} />}
-            </button>
+            </button> */}
             <button
               onClick={() => handleForward()}
               className="backfront-btn back-btn"
@@ -119,7 +157,7 @@ const MusicPlayer = () => {
             className="seek"
             type="range"
             min="0"
-            max={audioRef.current ? audioRef.current.duration : 0}
+            max={audioRef.current ? audioRef?.current?.duration : 0}
             value={currentTime}
             onChange={handleSeek}
           />
@@ -132,8 +170,8 @@ const MusicPlayer = () => {
           <button>
             <LuShuffle size={18} />
           </button>
-          <button>
-            <LuRepeat2 size={18} />
+          <button onClick={toggleLoop}>
+            <LuRepeat2 color={audioLoop ? 'green' : ''} size={18} />
           </button>
           <button onClick={toggleMute}>
             {isMuted ? (
