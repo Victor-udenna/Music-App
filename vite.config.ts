f652cf77cbffe0b702cb21cec491abde0f766658
Victor udenna <victorudenna@gmail.com>
2024-05-12 15:25:11 +0100

    fix(pwa icon)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa';
 
+
 const manifestForPlugin: Partial<any> = {
   registerType: "prompt",
   includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
@@ -11,23 +12,23 @@ const manifestForPlugin: Partial<any> = {
     description: "Music screeaming platform",
     icons: [
       {
-        src: "/android-chrome-192x192.png",
+        src: "./src/assets/icons/logo.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/android-chrome-512x512.png",
+        src: "./src/assets/icons/logo.png",
         sizes: "512x512",
         type: "image/png",
       },
       {
-        src: "/apple-touch-icon.png",
+        src: "./src/assets/icons/logo.png",
         sizes: "180x180",
         type: "image/png",
         purpose: "apple touch icon",
       },
       {
-        src: "/maskable_icon.png",
+        src: "./src/assets/icons/logo.png",
         sizes: "225x225",
         type: "image/png",
         purpose: "any maskable",
